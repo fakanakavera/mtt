129e3306a067d81d1577458718bd9463ba77b285
fakanakavera <34803622+fakanakavera@users.noreply.github.com>
2024-06-18 22:27:49 +0900

    templates added

--- metate/urls.py
+++ metate/urls.py
@@ -1,9 +1,12 @@
-# metate/urls.py
-
 from django.urls import path
-from . import views
+from .views import (
+    StoneListView, StoneDetailView, StoneCreateView, StoneUpdateView, StoneDeleteView
+)
 
 urlpatterns = [
-    # Define your URL patterns here
-    path('', views.index, name='index'),
+    path('stones/', StoneListView.as_view(), name='stone_list'),
+    path('stones/<int:pk>/', StoneDetailView.as_view(), name='stone_detail'),
+    path('stones/new/', StoneCreateView.as_view(), name='stone_create'),
+    path('stones/<int:pk>/edit/', StoneUpdateView.as_view(), name='stone_update'),
+    path('stones/<int:pk>/delete/', StoneDeleteView.as_view(), name='stone_delete'),
 ]
--- metate/views.py
+++ metate/views.py
@@ -1,6 +1,44 @@
-from django.shortcuts import render
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.views.generic import ListView, DetailView
+from django.urls import reverse_lazy
+from .models import Stone
 
-# Create your views here.
-def index(request):
-    # return blank response
-    return render(request, 'metate/index.html')
+class StoneListView(ListView):
+    model = Stone
+    template_name = 'metate/stone_list.html'
+
+class StoneDetailView(DetailView):
+    model = Stone
+    template_name = 'metate/stone_detail.html'
+
+class StoneCreateView(CreateView):
+    model = Stone
+    fields = ['name', 'size', 'design_number', 'description']
+    template_name = 'metate/stone_form.html'
+    success_url = reverse_lazy('stone_list')
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['cancel_url'] = reverse_lazy('stone_list')
+        return context
+
+class StoneUpdateView(UpdateView):
+    model = Stone
+    fields = ['name', 'size', 'design_number', 'description']
+    template_name = 'metate/stone_form.html'
+    success_url = reverse_lazy('stone_list')
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['cancel_url'] = reverse_lazy('stone_list')
+        return context
+
+class StoneDeleteView(DeleteView):
+    model = Stone
+    template_name = 'metate/stone_confirm_delete.html'
+    success_url = reverse_lazy('stone_list')
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['cancel_url'] = reverse_lazy('stone_list')
+        return context
