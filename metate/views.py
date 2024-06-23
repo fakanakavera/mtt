83bee2708da1aacbb9e5d2c1e4eb8db2d475f014
fakanakavera <34803622+fakanakavera@users.noreply.github.com>
2024-06-23 15:44:20 +0900

    invalid form

--- metate/views.py
+++ metate/views.py
@@ -2,6 +2,7 @@ from django.views.generic import ListView, DetailView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.urls import reverse_lazy
 from .models import Stone, Flange, StoneHandling
+from django.contrib import messages
 
 class StoneListView(ListView):
     model = Stone
@@ -89,6 +90,84 @@ class StoneHandlingCreateView(CreateView):
         context = super().get_context_data(**kwargs)
         context['cancel_url'] = reverse_lazy('stone_list')
         return context
+    
+    def form_valid(self, form):
+        stone = form.cleaned_data['stone']
+        action = form.cleaned_data['action']
+        flange = form.cleaned_data['flange']
+
+        # Check if the selected flange is associated with the selected stone
+        if flange and flange.stone != stone:
+            form.add_error(None, "The selected flange is not associated with the selected stone.")
+            return self.form_invalid(form)
+
+        # Perform additional checks and actions based on the action
+        if action == 'discarded' and stone.main_state not in ['DISCARDED']:
+            stone.main_state = 'DISCARDED'
+            stone.save()
+            if flange:
+                flange.stone = None
+                flange.save()
+            StoneHandling.objects.create(stone=stone, action='discarded', action_date=form.cleaned_data['action_date'])
+
+        elif action == 'shelved_with_flange' and stone.main_state == 'WITH_FLANGE_IN_SPINDLE':
+            stone.main_state = 'WITH_FLANGE'
+            stone.save()
+            if flange:
+                flange.current_status = 'STORED'
+                flange.save()
+            StoneHandling.objects.create(stone=stone, action='shelved_with_flange', action_date=form.cleaned_data['action_date'])
+
+        elif action == 'shelved_only_stone' and stone.main_state == 'WITH_FLANGE':
+            stone.main_state = 'BY_ITSELF'
+            stone.save()
+            if flange:
+                flange.stone = None
+                flange.current_status = 'STORED'
+                flange.save()
+            StoneHandling.objects.create(stone=stone, action='shelved_only_stone', action_date=form.cleaned_data['action_date'])
+
+        elif action == 'reinstated' and stone.main_state == 'WITH_FLANGE_IN_SPINDLE':
+            stone.main_state = 'WITH_FLANGE_IN_SPINDLE'
+            stone.save()
+            if flange:
+                flange.current_status = 'IN_USE'
+                flange.save()
+            StoneHandling.objects.create(stone=stone, action='reinstated', action_date=form.cleaned_data['action_date'])
+
+        elif action == 'mounted' and stone.main_state == 'BY_ITSELF':
+            stone.main_state = 'WITH_FLANGE'
+            stone.save()
+            if flange:
+                flange.stone = stone
+                flange.current_status = 'IN_USE'
+                flange.save()
+            StoneHandling.objects.create(stone=stone, action='mounted', action_date=form.cleaned_data['action_date'])
+
+        elif action == 'removed' and stone.main_state == 'WITH_FLANGE':
+            stone.main_state = 'BY_ITSELF'
+            stone.save()
+            if flange:
+                flange.stone = None
+                flange.current_status = 'STORED'
+                flange.save()
+            StoneHandling.objects.create(stone=stone, action='removed', action_date=form.cleaned_data['action_date'])
+
+        elif action == 'change_design_number':
+            new_design_number = self.request.POST.get('new_design_number')
+            if new_design_number:
+                stone.design_number = new_design_number
+                stone.save()
+                StoneHandling.objects.create(stone=stone, action='change_design_number', action_date=form.cleaned_data['action_date'])
+            else:
+                form.add_error(None, "Design number must be provided for this action.")
+                return self.form_invalid(form)
+
+        else:
+            form.add_error(None, "Invalid action for the current stone state or missing required information.")
+            return self.form_invalid(form)
+
+        return super().form_valid(form)
 
 class StoneHandlingListView(ListView):
     model = StoneHandling
