2ea854ec33f8549754c15891cda912338131af45
fakanakavera <34803622+fakanakavera@users.noreply.github.com>
2024-06-25 04:00:40 +0900

    asd

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='metate',
-    version='0.3.32',
+    version='0.3.33',
     packages=find_packages(),
     include_package_data=True,
     license='MIT License',  # Choose an appropriate license
