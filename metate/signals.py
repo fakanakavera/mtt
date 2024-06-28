19a043469a00e714fbe87b8c84b8d2e39e17a449
fakanakavera <34803622+fakanakavera@users.noreply.github.com>
2024-06-29 05:14:04 +0900

    asd

--- metate/signals.py
+++ metate/signals.py
@@ -13,7 +13,7 @@ def handle_stone_handling(sender, instance, created, **kwargs):
         return
 
     hinban_list = load_yaml(os.path.join(DIR, 'variables', 'hinbans.yaml'))
-    hinban_list = hinban_list['hinban_list']
+    hinban_list = hinban_list['hinban_list'][0]
     print(hinban_list)
     stone = instance.stone
     action = instance.action
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='metate',
-    version='0.4.30',
+    version='0.4.31',
     packages=find_packages(),
     include_package_data=True,
     license='MIT License',  # Choose an appropriate license
